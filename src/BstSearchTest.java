621665294c0f17d049ce41b7a12aa1732bd172a3
Gurpinder Gill <gill.gurpinder@student.greenriver.edu>
2026-09-23 16:04:25 -0700

    added left node test

--- src/BstSearchTest.java
+++ src/BstSearchTest.java
@@ -1,4 +1,7 @@
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.net.IDN;
+
 import org.junit.jupiter.api.Test;
 
 public class BstSearchTest {
@@ -10,4 +13,18 @@ public class BstSearchTest {
 
    }
 
+
+  @Test 
+  void ContainValueInLeftNode() {
+    BinaryTreeNode<Integer> root = 
+      new BinaryTreeNode<>(10,
+        new BinaryTreeNode<>(5),
+        new BinaryTreeNode<>(15));
+
+        assertTrue(BstSearch.contains(root, 5));
+
+
+  }
+ 
+      
   }
