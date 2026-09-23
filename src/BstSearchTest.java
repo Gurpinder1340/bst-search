b2a8dcd11385e5e3a76e0035ce71327f667bc3f0
Gurpinder Gill <gill.gurpinder@student.greenriver.edu>
2026-09-23 15:31:20 -0700

    added binary search tree method

--- src/BstSearchTest.java
+++ src/BstSearchTest.java
@@ -2,5 +2,15 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class BstSearchTest {
+ @Test 
+ void ContainsValueInRoot() {
+    BinaryTreeNode<Integer> root = new BinaryTreeNode<>(10);
+
+    assertTrue(BstSearch.contains(root,10));
+ }
+
+
+   
+  
 
 }
